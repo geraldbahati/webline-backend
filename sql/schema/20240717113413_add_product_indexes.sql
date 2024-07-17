40cc6c36b2d996a2219fc74e5a16afb0eb76005e
geraldbahati <journeytoharvard@gmail.com>
2024-07-17 12:02:11 +0300

    chore: Update sqlc version to v1.24.0

--- sql/schema/20240717113413_add_product_indexes.sql
+++ sql/schema/20240717113413_add_product_indexes.sql
@@ -1,9 +1,13 @@
 -- +goose Up
--- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
+-- SQL in section 'Up' is executed when this migration is applied.
+
+CREATE INDEX IF NOT EXISTS idx_products_search_keyword ON products USING gin(search_keyword);
+CREATE INDEX IF NOT EXISTS idx_products_name_description ON products (name, description);
+CREATE INDEX IF NOT EXISTS idx_products_is_active ON products (is_active);
 
 -- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
+-- SQL section 'Down' is executed when this migration is rolled back.
+
+DROP INDEX IF EXISTS idx_products_search_keyword;
+DROP INDEX IF EXISTS idx_products_name_description;
+DROP INDEX IF EXISTS idx_products_is_active;
