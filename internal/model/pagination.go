a1cb6fb6845fe4900b6ca2bdd63f0d80399c6120
geraldbahati <journeytoharvard@gmail.com>
2024-08-01 17:34:15 +0300

    Refactor PaginationResult struct field names to adhere to JSON naming conventions

--- internal/model/pagination.go
+++ internal/model/pagination.go
@@ -1,9 +1,9 @@
 package model
 
 type PaginationResult[T any] struct {
-	TotalCount int64 `json:"total_count"`
-	TotalPages int32 `json:"total_pages"`
+	TotalCount int64 `json:"totalCount"`
+	TotalPages int32 `json:"totalPages"`
 	Page       int32 `json:"page"`
-	PageSize   int32 `json:"page_size"`
+	PageSize   int32 `json:"pageSize"`
 	Data       T     `json:"data"`
 }
